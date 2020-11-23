62972a16c709731989d240052e6829d41a4beefb
JinEnHui <1791650297@qq.com>
2020-11-23 16:09:16 +0800

    秦乐完善api 跨域

--- vue.config.js
+++ vue.config.js
@@ -12,19 +12,19 @@ module.exports = {
         // 是否开启https协议
         https: false,
         // 配置跨域
-        // proxy: {
-        //     // 代理地址    需要代理的接口
-        //     [process.env.VUE_APP_BASE_API]: {
-        //         // 跨域地址    目标地址
-        //         target: process.env.VUE_APP_SERVICE_URL,
-        //         // 开启代理   是否跨域
-        //         changeOrigin: true,
-        //         // 重写路径
-        //         pathRewrite: {
-        //             ['^' + process.env.VUE_APP_BASE_API]: ''
-        //         }
-        //     }
-        // }
+        proxy: {
+            // 代理地址    需要代理的接口
+            [process.env.VUE_APP_BASE_API]: {
+                // 跨域地址    目标地址
+                target: process.env.VUE_APP_SERVICE_URL,
+                // 开启代理   是否跨域
+                changeOrigin: true,
+                // 重写路径
+                pathRewrite: {
+                    ['^' + process.env.VUE_APP_BASE_API]: ''
+                }
+            }
+        }
     },
     // 是否开启ESlint语法
     lintOnSave: false,
