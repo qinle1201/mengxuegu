6570ce8b5eb9ed1de9cb17f26b077ca9ff644d89
韩萍 <1098668705@qq.com>
2020-11-23 14:55:01 +0800

    hp-login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,61 @@
 <template>
-  <div>登录</div>
+  <div class="box">
+    <div class="login">
+      
+    </div>
+  </div>
 </template>
+
 <script>
 export default {
+  // 组件名称
+  name: "demo",
+  // 组件参数 接收来自父组件的数据
+  props: {},
+  // 局部注册的组件
+  components: {},
+  // 组件状态值
   data() {
     return {};
   },
-  created() {},
+  // 计算属性
   computed: {},
+  // 侦听器
+  watch: {},
+  // 组件方法
   methods: {},
+
+  beforeCreate() {},
+
+  created() {},
+
+  beforeMount() {},
+
+  mounted() {},
+
+  beforeUpdate() {},
+
+  updated() {},
+
+  activated() {},
+
+  deactivated() {},
+
+  beforeDestroy() {},
+
+  destroyed() {}
 };
-</script>
-<style lang="scss" scoped>
+</script> 
+
+<style  lang="scss" scoped>
+*{
+  margin: 0px;
+  padding: 0px;
+}
+.box{
+  width: 100%;
+  height: 100%;
+  background:url('http://mengxuegu.com:9999/img/login.b665435f.jpg') no-repeat;
+}
 </style>
+
