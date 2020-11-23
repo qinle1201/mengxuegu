acbf08c2aa130a988f960e952171ed6acd1c55e0
秦乐 <8085524+qin-music@user.noreply.gitee.com>
2020-11-23 18:38:57 +0800

    封装了面包屑

---
+++ src/components/crumbs/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="crumbs">
+    <!-- 封装面包屑导航 -->
+    <el-card class="box-card">
+      <span>{{ $route.meta.title }}</span>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {},
+};
+</script>
+
+<style scoped lang="scss">
+.crumbs {
+  span {
+    border-left: 3px solid green;
+    padding-left: 5px;
+  }
+}
+</style>
